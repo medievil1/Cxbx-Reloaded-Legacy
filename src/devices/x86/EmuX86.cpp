e862dbda5467345e966f497a47a4c81934c956fe
medievil1 <neo_waxworks@yahoo.com>
2026-05-28 20:55:30 -0400

    fix: remove duplicate for-loop block introduced by DecodeOpcode profiler edit

--- src/devices/x86/EmuX86.cpp
+++ src/devices/x86/EmuX86.cpp
@@ -3017,23 +3017,6 @@ bool EmuX86_DecodeException(LPEXCEPTION_POINTERS e)
 			}
 		}
 
-	// Execute op-codes until we hit an unhandled instruction, or an error occurs
-	//while (true)
-	// TODO: Find where the weird memory addresses come from when using the above case
-	// There is obviously something wrong with one or more of our instruction implementations
-	// For now, we only execute one instruction at a time...
-	for (int x=0;x<1;x++)
-	{
-		if (!EmuX86_DecodeOpcode((uint8_t*)e->ContextRecord->Eip, info)) {
-			EmuLog(LOG_LEVEL::WARNING, "Error decoding opcode at 0x%08X", e->ContextRecord->Eip);
-			assert(false);
-			return false;
-		}
-
-		LOG_CHECK_ENABLED(LOG_LEVEL::DEBUG) {
-			EmuX86_DistormLogInstruction((uint8_t*)e->ContextRecord->Eip, info, LOG_LEVEL::DEBUG);
-		}
-
 		switch (info.opcode) { // Keep these cases alphabetically ordered and condensed
 			case I_ADD:
 				if (EmuX86_Opcode_ADD(e, info)) break;
